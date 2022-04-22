d5787a7b46ae3da4b13fd6237e425c4c328cb94b
YunH <soimar@naver.com>
2022-04-22 22:44:54 +0900

    Feat: add calendar(),swiper for main section in index.js

--- index.js
+++ index.js
@@ -1,34 +1,92 @@
-const swiper = new Swiper('.swiper', {
-
+// ================================== 스와이퍼 =====================================
+const swiperVisual = new Swiper('#visual', {
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  oopAdditionalSlides: 1,
+  watchSlidesProgress: true,
+  centeredSlides: true,
   slidesPerView: '1',
   loop: true,
-  autoplay: { 
-          delay: 2000,
-          disableOnInteraction: true, 
-        },
-  autoHeight : true,
-  watchOverflow : true,
   effect: "fade",
   fadeEffect: {
     crossFade: true,
   },
-
-  watchSlidesProgress: true,
-
+  autoHeight: true,
   pagination: {
     el: '.swiper-pagination',
-    type : 'bullets',
+    type: 'bullets',
     clickable: true,
-    
-    renderBullet: function (index, className) { 
+    renderBullet: function (index, className) {
       return `<span class="${className}">${index + 1}</span>`
     },
   },
 });
 
 
+const swiperMainContents = new Swiper('#main-contents',{
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  // spaceBetween: 10,
+  // oopAdditionalSlides: 1,
+  loop: true,
+  centeredSlides: true,
+  // slidesPerView: '1',
+  // slideToClickedSlide: true,
+  // effect: 'coverflow',
+  // coverflowEffect: {
+  //   rotate : 20, 
+  //   stretch : 0, 
+  //   depth : 50,
+    
+  // }
+})
 
-  const visualSwiper = document.querySelector('.swiper.visual.container').swiper;
 
-  visualSwiper.slideNext();
 
+
+
+// ================================== 달력 ===================================== 
+
+function calendar() {
+  //Date 인스턴스 생성
+  const date = new Date();
+  //올해, 이번달, 이번달의 마지막날, 오늘
+  const thisYear = date.getFullYear();
+  const thisMonth = ('0' + (date.getMonth() + 1)).slice(-2);
+  const days = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  const today = date.getDate();
+
+  //오늘 확인
+  console.log(today)
+
+  //년,월,일 el 가져오기
+  const getYearEl = document.querySelector('.cal-show-year');
+  const getMonthEl = document.querySelector('.cal-show-month');
+  const getDateEl = document.querySelector('.main-contents__visual--cal-date');
+
+  //년, 월 값을 화면에 넣기
+  getYearEl.textContent = thisYear;
+  getMonthEl.textContent = thisMonth;
+
+  // 해달 월 일수 나열
+  for (let i = 0; i < days.getDate(); i += 1) {
+    //일 span el 생성
+    const createListEl = document.createElement('span');
+    //span을 div에 붙이기
+    getDateEl.appendChild(createListEl)
+    //붙인 span에 날짜 넣기 
+    createListEl.innerText = i + 1;
+
+    //오늘 클래스 추가
+    if (today === i + 1) {
+      createListEl.classList.add('today');
+    }
+  }
+
+}
+calendar()
