0702ac86b7a1dd8afcb2854991ee7cbda87d9bdd
YunH <soimar@naver.com>
2022-04-28 16:43:04 +0900

    Feat: Create menu feature to add on index.html through index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// =========================== aos init ============================================
+// =============================== aos init ===============================
 AOS.init();
 
-// ================================== 스와이퍼 =====================================
+// =============================== 스와이퍼 ===============================
 
 //visual container영역
 const swiperVisual = new Swiper(".swiper.visual__container", {
@@ -51,7 +51,7 @@ const swiperMainContentsSNS = new Swiper(".swiper.main-contents__sns--img", {
   slideToClickedSlide: true,
 });
 
-// ================================== 달력 =====================================
+// =============================== 달력 ===============================
 
 function calendar() {
   //Date 인스턴스 생성
@@ -88,7 +88,7 @@ function calendar() {
 }
 calendar();
 
-//============================ 푸터 패밀리 사이트 버튼 =========================
+// =============================== 푸터 패밀리 사이트 버튼 ===============================
 
 const listFamilyEl = document.querySelector(".lists-etc-family a");
 //클릭 확인용 변수
@@ -107,7 +107,7 @@ listFamilyEl.addEventListener("click", () => {
   isActive = !isActive;
 });
 
-//========================= 스크롤 이동 버튼 ===================================
+// =============================== 스크롤 이동 버튼 ===============================
 
 const scrollBtnEl = document.querySelector(".scroll__btn");
 
@@ -137,3 +137,62 @@ scrollBtnEl.addEventListener("click", () => {
     scrollTo: 0,
   });
 });
+
+// =============================== 헤더 메뉴 넣기 ===============================
+
+//메뉴 데이터
+const headerData = [
+  {
+    name: "경기생활문화센터",
+    list: ["센터소개", "조직도", "사업안내", "오시는길"],
+  },
+  {
+    name: "경기도생활문화",
+    list: ["우리지역 생활문화공간", "생활문화찾기"],
+  },
+  {
+    name: "기록자료",
+    list: ["영상채널", "발간도서"],
+  },
+  {
+    name: "웹진",
+    list: ["소개", "웹진"],
+  },
+  {
+    name: "소통",
+    list: ["공지사항", "자주묻는질문", "1:1문의"],
+  },
+];
+
+//헤더 영역에 메뉴 추가하기
+for (const data in headerData) {
+  //ul태그에 li 추가
+  const mainManuUl = document.querySelector("ul.header__menu--list");
+  const newMainMenuLi = document.createElement("li");
+  //li의 내용
+  const mainMenu = `
+    <a class="name" href="javascript:void(0);">
+    <h2>${headerData[data].name}</h2></a>
+    <div class="contents hide"><ul class="item${data}"></ul></div>
+  `;
+  //li에 내용,class추가
+  newMainMenuLi.classList.add("list--item");
+  newMainMenuLi.innerHTML = mainMenu;
+  //적용
+  mainManuUl.appendChild(newMainMenuLi);
+
+  //메인메뉴 안 sub li 넣기
+  if (headerData[data].name !== data) {
+    for (let i = 0; i < headerData[data].list.length; i += 1) {
+      //sub ul에 li추가
+      const subMenuUl = document.querySelector(`ul.item${data}`);
+      const newSubMenuLi = document.createElement("li");
+      //sub li의 내용
+      const subMenu = `${headerData[data].list[i]}`;
+      //sub li에 내용 추가
+      newSubMenuLi.innerHTML = subMenu;
+      //적용
+      subMenuUl.appendChild(newSubMenuLi);
+    }
+  }
+}
